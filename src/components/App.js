26f3e5177cf55f19a12dd4e270a770a8b2a3255b
G3MM4 <gemmvaughan@gmail.com>
2018-09-08 22:55:41 +0200

    getting state n props

--- src/components/App.js
+++ src/components/App.js
@@ -9,20 +9,31 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {			
-			repos: []			
-		}		
-	};   
+			repos: [],						
+		}	
+		
+		this.getRepos=this.getRepos.bind(this);
+	}  
 
 	componentDidMount(){
+		this.getRepos();			
+	}
+	
+	getRepos(){
 		fetch(apiUrl)
-			.then ((response)=>response.json())		
-			.then ((jsonData)=>{
-				console.log('App jsondata', jsonData);
-				this.setState({
-					repos: jsonData		//y de aquí no sale el estado
-				})							
-			});console.log('estado1', this.state);				
-	};	
+		.then ((response)=>response.json())		
+		.then ((jsonData)=>{
+			console.log('App jsondata', jsonData);
+			let repos = [];
+			for (let i = 0; i < jsonData.length; i++){
+				repos[i] = {...jsonData[i]};
+			}
+			this.setState({
+				repos: repos 
+			});							
+		});	
+	}
+	
 	
 
 	render(){
@@ -30,14 +41,15 @@ class App extends Component {
 		return (
 			<div>
 				<input type="text" placeholder="repositorio1"/>
-				<Search />
-				<RepoList />			
+				<Search repos={this.state.repos}/>
+				<RepoList repos={this.state.repos}/>	
+					
 			</div>
 		)
 	}
 }
 		
-console.log('App props',this.props)
+
 export default App;
 
 
--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -5,17 +5,16 @@ import Repo from './Repo'; //Repo es mi hija
 class RepoList extends Component {
     
     render() { 
-        console.log('RepoList props', this.props)//undefined para variar
+        console.log('RepoList props', this.props)
 
         return ( 
             <Fragment>
                 <main>
                     <ul>
-                        <Repo 
-                            // name={name}
-                            // description={description}
-                            // languages={languages}
-                        />               
+                        <li>
+                            
+                            <Repo repos={this.props.repos}/>
+                        </li>           
                     </ul>
                 </main>
             </Fragment>
