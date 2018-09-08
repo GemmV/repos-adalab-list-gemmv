ecf296fe2b810af22f6c069d02b93b8c076b8d60
G3MM4 <gemmvaughan@gmail.com>
2018-09-08 21:49:02 +0200

    basic structure

---
+++ src/components/RepoList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Repo from './Repo'
+
+
+class RepoList extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <Repo />
+            </div> );
+    }
+}
+ 
+export default RepoList;
